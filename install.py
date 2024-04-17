18c1a6c7178cbd1b34cb1f1eaa29f8e41fd96ad0
tritom88 <35511186+tritom88@users.noreply.github.com>
2024-04-17 13:33:51 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-import os
 import subprocess
 
-os.environ['PIP_BREAK_SYSTEM_PACKAGES'] = '1'
-subprocess.call([ 'pip', 'install', 'inquirer', '-q' ])
+subprocess.call([ 'pip', 'install' , 'inquirer', '-q' ])
 
 from facefusion import installer
 
